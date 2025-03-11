22f50cdea5ece43f252f87b5e9dc122f948b02d6
Alberto <albertogagoprogramacion@gmail.com>
2025-03-11 14:41:50 +0100

    Initial Commit

---
+++ src/main/java/com/teamCacahuete/GymRatz/GymRatzApplication.java
@@ -0,0 +1,13 @@
+package com.teamCacahuete.GymRatz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymRatzApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymRatzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamCacahuete/GymRatz/GymRatzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamCacahuete.GymRatz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymRatzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
